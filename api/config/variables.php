54cc607641e7d34d9ee6582fab667b4ca4dc764f
Lauty128 <lautarosilverii@gmail.com>
2023-08-30 17:46:43 -0300

    database config and variables defined

--- api/config/variables.php
+++ api/config/variables.php
@@ -4,4 +4,9 @@
     define('DB_SERVER','localhost');
     define('DB_NAME','fio_project');
     define('DB_USER','root');
-    define('DB_PASSWORD','');
+    define('DB_PASSWORD','12052003');
+
+//------ DEFAULT VALUES
+    #Aqui definimos los valores por defecto que tomaremos en el codigo
+    define('PAGE', 0);
+    define('LIMIT', 30);
