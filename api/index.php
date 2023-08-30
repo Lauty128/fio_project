a8131c62ad3d442cb7458ad26985ed447fbf533b
Lauty128 <lautarosilverii@gmail.com>
2023-08-30 17:48:24 -0300

    enrutamiento de proveedores basico definido

--- api/index.php
+++ api/index.php
@@ -2,11 +2,27 @@
     # ------------ Importar código de flightphp
     require 'vendor/flightphp/flight/Flight.php';
 
-    #**************** ENDPOINTS ***********************
+    # Importar variables
+    require_once 'config/variables.php';
 
+    # ------------ Importar los controladores
+    require 'controller/proveedores.controller.php';
+    
+
+    #******************* ENDPOINTS **************************
     # --- Obtener todos los proveedores
     Flight::route('/proveedores', function(){
-        echo 'Hola mundo desde "Proveedores"';
+
+        # Si existe el parametro page,  entonces se usa ese valor, si no el definido en el archivo variables.php
+        $page = (isset($_GET['page']) && ($_GET['page'] > 0)) ? ($_GET['page'] - 1) : constant('PAGE');
+        # Si existe el parametro limit, entonces se usa ese valor, si no el definido en el archivo variables.php
+        $limit = (isset($_GET['limit'])) ? $_GET['limit'] : constant('LIMIT');
+
+        # Almacenar el resultado de la funcion getAll() de la clase ProveedoresControllers en $data
+        $data = ProveedoresController::getAll($page, $limit);
+        
+        # Ejecutar la funcion JSON de FlightPHP para devolver un JSON formateado
+        Flight::json($data);
     });
     
     # --- Obtener un proveedor
