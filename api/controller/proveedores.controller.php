b30bad8c56d4edbb6ccdb5558f994034eb7dfe03
Lauty128 <lautarosilverii@gmail.com>
2023-09-02 09:02:47 -0300

    modelo y controller basicos para consultas de proveedores añadidos

--- api/controller/proveedores.controller.php
+++ api/controller/proveedores.controller.php
@@ -5,24 +5,55 @@
 
     class ProveedoresController{
     
-        static function getAll(int $page, int $limit, $category = null)
+        static function getAll(int $page, int $limit, bool $equipments)
         {
             # Obtenemos el offset multiplicando la $page por el $limit
             $offset = $page * $limit;
-            # Esto es para que no tire error de variable sin usar
-            $category = $category;
 
             # Almacenamos el resultado de la funcion getAll() mandando los parametros que pide
-            $data = ProveedoresModel::getAll($offset, $limit);
+            $data = ProveedoresModel::getAll($offset, $limit, $equipments);
             
             # Retornamos el valor para usarlo en index.php
             return $data;
+            
         }
         
-        static function getAll_byEquipment($equipment, $page = 0, $limit = 30){
-            # Use the PDO variable of the line 6
-            global $PDO;
+        static function getEquipments(int $id, int $page, int $limit)
+        {
+            # Obtenemos el offset multiplicando la $page por el $limit
+            $offset = $page * $limit;
 
+            # Almacenamos el resultado de la funcion getEquipments() mandando los parametros que pide
+            $response = ProveedoresModel::getEquipments($id, $offset, $limit);
+                
+            # Retornamos el valor para usarlo en index.php
+            return $response;
+        }
+
+        static function getOne(int $id)
+        {            
+            $provider = ProveedoresModel::getOne($id);
+            
+            if(isset($provider['Error'])){
+                return $provider;
+            }
+
+
+            if(count($provider) == 1){
+                $equipments = ProveedoresModel::getEquipments($id);
+
+                return [
+                    ...$provider[0],
+                    "equipos" => $equipments
+                ];
+            }
+            else{
+                return [
+                    "Error"=>000,
+                    "Message"=>"No existe el proveedor buscado"
+                ];
+            }
+            
         }
 
     }
--- api/model/proveedores.model.php
+++ api/model/proveedores.model.php
@@ -4,17 +4,26 @@
     $database = new Database();
     $PDO = $database->connect();
 
-    class ProveedoresModel{
+    class ProveedoresModel{ 
 
-        static function getAll($offset, $limit)
+        static function getAll($offset, $limit, bool $equipments)
         {
             # llamamos a la variable global $PDO
             global $PDO;
-            
+
             # Si $PDO no es un PDOException, enonces la conexion es correcta y ejecutamos lo siguiente
             if(get_class($PDO) !== 'PDOException'){
                 # Creamos la query con los parametros recibidos
-                $sql = 'SELECT * FROM proveedor LIMIT :limit OFFSET :offset';
+                $sql = ($equipments)
+                ?   'SELECT p.*, count(pe.cod_proveedor) as equipos
+                    FROM proveedor p
+                    JOIN proveedor_equipo pe ON p.cod_proveedor = pe.cod_proveedor
+                    group by p.cod_proveedor'
+                :   'SELECT * FROM proveedor';
+                    //ORDER BY equipos DESC'
+                
+                # Agregar la paginación
+                $sql .= ' LIMIT :limit OFFSET :offset';
 
                 # Preparamos la query con el string generado
                 $query = $PDO->prepare($sql);
@@ -31,6 +40,88 @@
                 # Retornamos el valor para usarlo en proveedores.model.php
                 return $data;
             }
+            else{
+                return [
+                    'Error'=>500,
+                    'Message'=>'Ocurrio un error al conectarse a la base de datos',
+                    'Error-Message' => $PDO->getMessage()
+                ];
+            }
+        }
+
+        static function getEquipments(int $id, int $offset = 0, int $limit = 10)
+        {
+            # llamamos a la variable global $PDO
+            global $PDO;
+
+            # Si $PDO no es un PDOException, enonces la conexion es correcta y ejecutamos lo siguiente
+            if(get_class($PDO) !== 'PDOException'){
+                # Creamos la query con los parametros recibidos
+                $sql="SELECT e.nombre, c.nombre as categoria
+                        FROM proveedor_equipo pe
+                        JOIN equipo e ON pe.cod_equipo = e.cod_equipo
+                        JOIN categoria c ON e.cod_categoria = c.cod_categoria
+                        WHERE pe.cod_proveedor = :id
+                        LIMIT :limit
+                        OFFSET :offset ";
+
+                # Preparamos la query con el string generado
+                $query = $PDO->prepare($sql);
+
+                # Definimos los valores de los parametros
+                $query->bindParam(':id', $id);
+                $query->bindParam(':limit', $limit);
+                $query->bindParam(':offset', $offset);
+                
+                # Ejecutamos  la cnsulta
+                $query->execute();
+                # Obtenemos un array con los datos recibidos
+                $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+                # Retornamos el valor para usarlo en proveedores.model.php
+                return $data;
+            }
+            else{
+                return [
+                    'Error'=>500,
+                    'Message'=>'Ocurrio un error al conectarse a la base de datos',
+                    'Error-Message' => $PDO->getMessage()
+                ];
+            }
+        }
+        
+        static function getOne(int $id)
+        {
+            # Use the PDO variable of the line 6
+            global $PDO;
+        
+            # Si $PDO no es un PDOException, enonces la conexion es correcta y ejecutamos lo siguiente
+            if(get_class($PDO) !== 'PDOException'){
+                # Creamos la query con los parametros recibidos
+                $sql="SELECT * FROM proveedor WHERE cod_proveedor = :id";
+
+                # Preparamos la query con el string generado
+                $query = $PDO->prepare($sql);
+
+                # Definimos los valores de los parametros
+                $query->bindParam(':id', $id);
+                
+                # Ejecutamos  la cnsulta
+                $query->execute();
+                # Obtenemos un array con los datos recibidos
+                $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+                # Retornamos el valor para usarlo en proveedores.model.php
+                return $data;
+            }
+            else{
+                return [
+                    'Error'=>500,
+                    'Message'=>'Ocurrio un error al conectarse a la base de datos',
+                    'Error-Message' => $PDO->getMessage()
+                ];
+            }
+            
         }
 
     }
