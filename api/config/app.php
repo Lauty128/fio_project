4511afbf6bd8021e7acb6356b36e7cc13b20ad4a
Lauty128 <lautarosilverii@gmail.com>
2023-09-22 08:29:23 -0300

    Contraseña de la base de datos eliminada

--- api/config/app.php
+++ api/config/app.php
@@ -15,7 +15,7 @@
     define('DB_SERVER','localhost');
     define('DB_NAME','fio_project');
     define('DB_USER','root');
-    define('DB_PASSWORD','12052003');
+    define('DB_PASSWORD','');
 
 //------ DEFAULT VALUES
     #Aqui definimos los valores por defecto que tomaremos en el codigo
