95aea159baa8057ee145ca231f9f564a0dbe3f2b
Lauty128 <lautarosilverii@gmail.com>
2023-09-27 08:41:27 -0300

    error con fetch solucionado(lo cambie por fetchALL)

--- api/model/equipments.model.php
+++ api/model/equipments.model.php
@@ -127,7 +127,7 @@
                     # Ejecutamos la consulta
                     $query->execute();
                     # Obtenemos un array con los datos recibidos
-                    $data = $query->fetch(PDO::FETCH_ASSOC);
+                    $data = $query->fetchAll(PDO::FETCH_ASSOC);
                 
                 # Retornamos el valor para usarlo en proveedores.model.php
                 return $data;
@@ -205,7 +205,7 @@
             # Ejecutamos  la cnsulta
             $query->execute();
             # Obtenemos un array con los datos recibidos
-            $data = $query->fetchAll(PDO::FETCH_ASSOC);
+            $data = $query->fetch(PDO::FETCH_ASSOC);
             
             # Retornamos el valor para usarlo en proveedores.model.php
             return $data;
