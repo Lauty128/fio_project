75458b38d21a1a37e20f668cc55a5a8e01574d89
Lauty128 <lautarosilverii@gmail.com>
2023-10-02 17:11:09 -0300

    ligero cambio en controlador de equipos

--- api/controller/equipments.controller.php
+++ api/controller/equipments.controller.php
@@ -12,43 +12,43 @@
     class EquipmentsController{
         
         static function getAll() { 
-            //-------------- Detectar queries
-                # Si existen los parametros toman su valor, sino el valor por defecto en las configuraciones
-                $page = (isset($_GET['page']) && ($_GET['page'] > 0)) ? ($_GET['page'] - 1) : constant('PAGE');
-                $limit = (isset($_GET['limit'])) ? $_GET['limit'] : constant('LIMIT');
+        //-------------- Detectar queries
+            # Si existen los parametros toman su valor, sino el valor por defecto en las configuraciones
+            $page = (isset($_GET['page']) && ($_GET['page'] > 0)) ? ($_GET['page'] - 1) : constant('PAGE');
+            $limit = (isset($_GET['limit'])) ? $_GET['limit'] : constant('LIMIT');
 
-            //-------------- Manipular queries
-                # Definimos un orden por defecto o el recibido por los parametros
-                $order = (isset($_GET['order'])) ? formaterOrder($_GET['order']) : constant('ORDER');
-                
-                # Formateamos las opciones de busqueda recibidas por parametro
-                $options = formaterOptionsForEquipments($_GET ?? []);
-                
-                # Obtenemos el offset multiplicando la $page por el $limit
-                $offset = $page * $limit;
+        //-------------- Manipular queries
+            # Definimos un orden por defecto o el recibido por los parametros
+            $order = (isset($_GET['order'])) ? formaterOrder($_GET['order']) : constant('ORDER');
             
-            //-------------- Enviar variables para ejecutar la consulta
-                # Almacenamos el resultado de la funcion getAll() mandando los parametros que pide
-                $data = EquipmentsModel::getAll($offset, $limit, $order, $options);
+            # Formateamos las opciones de busqueda recibidas por parametro
+            $options = formaterOptionsForEquipments($_GET ?? []);
+            
+            # Obtenemos el offset multiplicando la $page por el $limit
+            $offset = $page * $limit;
+        
+        //-------------- Enviar variables para ejecutar la consulta
+            # Almacenamos el resultado de la funcion getAll() mandando los parametros que pide
+            $data = EquipmentsModel::getAll($offset, $limit, $order, $options);
 
-                # Si la cantidad de elementos es menor al limite o igual a cero, no se consulta en la base de datos el total de elementos
-                $total = ((count($data) < $limit) && $page == 0)
-                    ? count($data)
-                    : EquipmentsModel::getTotal($options);
-                
-                if(is_int($total) && !isset($data['Error'])){
-                    return [
-                        'page' => ((int)$page + 1),
-                        'limit' => (int)$limit,
-                        'hasNextPage' => ((($page + 1) * $limit) < $total),
-                        'hasPrevPage' => (($page - 1) >= 0),
-                        'total' => (int)$total,
-                        'data' => $data
-                    ];
-                }
-                
-                // Si accede aqui es porque ocurrio un error
-                return $data;
+            # Si la cantidad de elementos es menor al limite o igual a cero, no se consulta en la base de datos el total de elementos
+            $total = ((count($data) < $limit) && $page == 0)
+                ? count($data)
+                : EquipmentsModel::getTotal($options);
+            
+            if(!isset($data['Error'])){
+                return [
+                    'page' => ((int)$page + 1),
+                    'limit' => (int)$limit,
+                    'hasNextPage' => ((($page + 1) * $limit) < $total),
+                    'hasPrevPage' => (($page - 1) >= 0),
+                    'total' => (int)$total,
+                    'data' => $data
+                ];
+            }
+            
+            // Si accede aqui es porque ocurrio un error
+            return $data;
         }
 
         static function getProviders(int $id)
