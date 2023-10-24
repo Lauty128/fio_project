cf03968fe433df173b100f435cefd17a503aa8e3
Lauty128 <lautarosilverii@gmail.com>
2023-10-24 11:35:45 -0300

    'Authorization' header changed by 'authorization'

--- api/middleware/auth.php
+++ api/middleware/auth.php
@@ -6,7 +6,7 @@ class Auth{
 
     static function VerifyAuthenication(): bool
     {
-        $token = getallheaders()['Authorization'] ?? '';
+        $token = getallheaders()['authorization'] ?? '';
         return $token === ACCES_TOKEN;
         // return in_array($host, ALLOWED_HOSTS) || $token === ACCES_TOKEN;
     }
