2467ec8566cebfcb897d5829673e9e5836f1c628
Lautaro Silverii <lautarosilverii@gmail.com>
2024-09-27 09:17:17 -0300

    Cabeceras INSERT INTO actualizadas para la nueva BD

--- api/src/utils/backup/Reading.php
+++ api/src/utils/backup/Reading.php
@@ -4,10 +4,10 @@ namespace App\Util\Backup;
 
 class Reading{
 
-    const HEADER__PROVIDER = "INSERT INTO `provider` (`providerID`, `name`, `mail`, `web`, `address`, `phone`) VALUES \n";
-    const HEADER__EQUIPMENT = "INSERT INTO `equipment` (`equipmentID`, `name`, `categoryID`, `umdns`, `description`, `price`, `specifications`) VALUES \n";
-    const HEADER__CATEGORY = "INSERT INTO `category` (`categoryID`, `name`) VALUES \n";
-    const HEADER__PROVIDER_EQUIPMENT = "INSERT INTO `provider_equipment` (`providerID`, `equipmentID`) VALUES \n";
+    const HEADER__PROVIDER = "INSERT INTO `users` (`id`, `name`, `email`, `web`, `address`, `phone`) VALUES \n";
+    const HEADER__EQUIPMENT = "INSERT INTO `equipments` (`id`, `name`, `id_category`) VALUES \n";
+    const HEADER__CATEGORY = "INSERT INTO `categories` (`id`, `name`) VALUES \n";
+    const HEADER__PROVIDER_EQUIPMENT = "INSERT INTO `providers_equipment` (`id_provider`, `id_equipment`) VALUES \n";
   
     static function createCategoriesQuery(array $array){
       $Query = self::HEADER__CATEGORY;
@@ -35,8 +35,7 @@ class Reading{
     static function createEquipmentsQuery(array $array){
       $Query = self::HEADER__EQUIPMENT;
         for ($i=1; $i < count($array); $i++) { 
-          $price = $array[$i][5] ?? 'NULL';
-          $Query .= "(".$array[$i][0].", '".$array[$i][1]."', ".$array[$i][2].", '".$array[$i][3]."', '".$array[$i][4]."', ".$price.", NULL),\n";
+          $Query .= "(".$array[$i][0].", '".$array[$i][1]."', 1),\n";
         }
   
       return substr($Query,0, (strlen($Query) - 2)).";";
